c14ea923df17f78da3d65c1c9dc5c0fa3f96e298
Emre Erarslan <emrerars@gmail.com>
2025-02-28 10:38:34 +0300

    added bg-color to view component

--- components/View.tsx
+++ components/View.tsx
@@ -18,7 +18,7 @@ const View = async ({ id }: { id: string }) => {
   );
 
   return (
-    <div className="flex justify-end items-center mt-5 fixed bottom-3 right-3 border-2 px-2 py-1 rounded-full">
+    <div className="flex justify-end items-center mt-5 fixed bottom-3 right-3 border-2 px-2 py-1 rounded-full bg-gray-100">
       <div className="absolute -top-1 -right-2">
         <Ping />
       </div>
