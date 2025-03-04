d96d006dc169bbdc77a00d15b185a4c805103c91
Emre Erarslan <emrerars@gmail.com>
2025-03-04 12:42:52 +0300

    changing location of category for trying to fix category bug

--- app/(root)/proje/[id]/page.tsx
+++ app/(root)/proje/[id]/page.tsx
@@ -81,7 +81,16 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
               <Link
                 href={`/?query=${project.category?.toLowerCase()}`}
-                className="text-sm px-4 py-1.5 bg-gray-50 text-gray-600 rounded-full hover:bg-gray-100 transition-colors hover:shadow-sm"
+                className="text-sm px-4 py-1.5 bg-gray-50 text-gray-600 rounded-full hover:bg-gray-100 transition-colors hover:shadow-sm hidden sm:block"
+              >
+                {project.category}
+              </Link>
+            </div>
+
+            <div className="sm:hidden mt-3">
+              <Link
+                href={`/?query=${project.category?.toLowerCase()}`}
+                className="text-sm px-4 py-1.5 bg-gray-50 text-gray-600 rounded-full hover:bg-gray-100 transition-colors hover:shadow-sm inline-block"
               >
                 {project.category}
               </Link>
